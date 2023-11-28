0104a65bcde57a183582869908564e785b1e1829
Vector <kirioza000@gmail.com>
2023-11-28 17:40:57 +0200

    Added vertex_shader and fragment_shader in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,36 @@
 
 #include <iostream>
 
+GLfloat point[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+const char* vertex_shader =
+"#version 460\n" 
+"layout(location = 0) in vec3 vertex_position;" 
+"layout(location = 1) in vec3 vertex_color;" 
+"out vec3 color" 
+"void main() {" 
+"   color = vertex_color;" 
+"   gl_Position = vec4(vertex_position, 1.0);" 
+"}";
+
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;" 
+"out vec4 frag_color;" 
+"void main() {"
+"   frag_color = vec4(color, 1.0);" 
+"}";
+
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
 
